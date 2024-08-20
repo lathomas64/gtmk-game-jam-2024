50a9598363609f661d69f8a1fc33529ae5f832d7
Imani <lathomas64@gmail.com>
2024-08-20 08:38:03 -0400

    randomize starting requests a little 1-3 requests

--- src/main.py
+++ src/main.py
@@ -3,12 +3,14 @@ from UrsinaExts.extended_ursina import ExtendedUrsina, window
 from planet import *
 from hud import HUD
 from request import Request
+import random
 
 if __name__ == "__main__":
     app = ExtendedUrsina()
     ui = HUD()
     #world = Planet()
-    Request()
+    for i in range(random.randint(1,3)):
+        Request()
 
     window.borderless = False
 
